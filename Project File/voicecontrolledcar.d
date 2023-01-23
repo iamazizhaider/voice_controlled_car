f50a2afc2377ee699fc6080efe9064d10d094339
Aziz Haider <76645480+iamazizhaider@users.noreply.github.com>
2023-01-23 23:11:43 +0500

    Add files via upload

---
+++ Project File/voicecontrolledcar.d
@@ -0,0 +1,5 @@
+.\voicecontrolledcar.o: VoiceControlledCar.c
+.\voicecontrolledcar.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\voicecontrolledcar.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
+.\voicecontrolledcar.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\voicecontrolledcar.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
